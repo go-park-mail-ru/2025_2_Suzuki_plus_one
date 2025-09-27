00e3d9e775151e3258e99f1a5a9b96f59790d5b5
sorrtory-desktop <62436169+sorrtory@users.noreply.github.com>
2025-09-27 04:31:11 +0300

    fix(githooks): change path to vault

--- githooks/pre-commit.d/check_leaks.sh
+++ githooks/pre-commit.d/check_leaks.sh
@@ -3,7 +3,7 @@
 # Check for unencrypted .env.vault file before committing
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-REPO_ROOT=$(dirname "$SCRIPT_DIR")
+REPO_ROOT=$(dirname "$(dirname "$SCRIPT_DIR")")
 
 vault_file="$REPO_ROOT/deployments/.env.vault"
 
@@ -14,9 +14,13 @@ hook_log() {
 	echo "${PREFIX}$1"
 }
 
+hook_log "Checking for unencrypted $vault_file."
+
 if [ -f "$vault_file" ] && ! grep -q '^$ANSIBLE_VAULT' "$vault_file"; then
     hook_log "You are probably trying to commit unencrypted $vault_file!"
     hook_log "Please encrypt it as soon as possible using:"
     hook_log "ansible-vault encrypt $vault_file --vault-password-file=vault_password.sh"
     exit 1
+else
+    hook_log "OK, no unencrypted vault file found."
 fi
