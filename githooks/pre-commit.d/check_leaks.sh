ac7fbc0a26dd5ba9770ce9811245010d14b30f20
sorrtory-desktop <62436169+sorrtory@users.noreply.github.com>
2025-09-28 10:37:27 +0300

    feat(githooks): add lists for leak checks

--- githooks/pre-commit.d/check_leaks.sh
+++ githooks/pre-commit.d/check_leaks.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# Check for unencrypted .env.vault file before committing
+# Check for unencrypted vault files before committing
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPO_ROOT=$(dirname "$(dirname "$SCRIPT_DIR")")
 
-vault_file="$REPO_ROOT/deployments/.env.vault"
-
+vault_files=(
+	"$REPO_ROOT/deployments/ansible.env.vault"
+	"$REPO_ROOT/deployments/prod.env.vault"
+)
 
 PREFIX="PRECOMMIT: LEAKS: "
 
@@ -14,13 +16,15 @@ hook_log() {
 	echo "${PREFIX}$1"
 }
 
-hook_log "Checking for unencrypted $vault_file."
+for vault_file in "${vault_files[@]}"; do
+	hook_log "Checking for unencrypted $vault_file."
 
-if [ -f "$vault_file" ] && ! grep -q '^$ANSIBLE_VAULT' "$vault_file"; then
-    hook_log "You are probably trying to commit unencrypted $vault_file!"
-    hook_log "Please encrypt it as soon as possible using:"
-    hook_log "ansible-vault encrypt $vault_file --vault-password-file=vault_password.sh"
-    exit 1
-else
-    hook_log "OK, no unencrypted vault file found."
-fi
+	if [ -f "$vault_file" ] && ! grep -q '^$ANSIBLE_VAULT' "$vault_file"; then
+		hook_log "You are probably trying to commit unencrypted $vault_file!"
+		hook_log "Please encrypt it as soon as possible using:"
+		hook_log "ansible-vault encrypt $vault_file --vault-password-file=vault_password.sh"
+		exit 1
+    fi
+done
+
+hook_log "OK, no unencrypted vault file found for ${vault_files[*]}."
