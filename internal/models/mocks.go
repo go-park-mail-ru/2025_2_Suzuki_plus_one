9356eaa94967688b85e25909b8bc8f980e27f486
Fadeev Arsenii <arsmfad@gmail.com>
2025-09-28 17:29:28 +0300

    feat(models): add films mocks

--- internal/models/mocks.go
+++ internal/models/mocks.go
@@ -19,6 +19,55 @@ func MockMovies() []Movie {
 			Genres:  []string{"Sci-Fi"},
 			Preview: "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
 		},
+		{
+			ID:      "3",
+			Title:   "Pulp Fiction",
+			Year:    1994,
+			Genres:  []string{"Crime", "Drama"},
+			Preview: "https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg",
+		},
+		{
+			ID:      "4",
+			Title:   "The Shawshank Redemption",
+			Year:    1994,
+			Genres:  []string{"Drama"},
+			Preview: "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
+		},
+		{
+			ID:      "5",
+			Title:   "Forrest Gump",
+			Year:    1994,
+			Genres:  []string{"Drama", "Romance"},
+			Preview: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg",
+		},
+		{
+			ID:      "6",
+			Title:   "Fight Club",
+			Year:    1999,
+			Genres:  []string{"Drama"},
+			Preview: "https://upload.wikimedia.org/wikipedia/en/f/fc/Fight_Club_poster.jpg",
+		},
+		{
+			ID:      "7",
+			Title:   "The Social Network",
+			Year:    2010,
+			Genres:  []string{"Biography", "Drama"},
+			Preview: "https://upload.wikimedia.org/wikipedia/en/8/8c/The_Social_Network_film_poster.png",
+		},
+		{
+			ID:      "8",
+			Title:   "Back to the Future",
+			Year:    1985,
+			Genres:  []string{"Adventure", "Comedy", "Sci-Fi"},
+			Preview: "https://upload.wikimedia.org/wikipedia/en/d/d2/Back_to_the_Future.jpg",
+		},
+		{
+			ID:      "9",
+			Title:   "The Silence of the Lambs",
+			Year:    1991,
+			Genres:  []string{"Crime", "Drama", "Thriller"},
+			Preview: "https://upload.wikimedia.org/wikipedia/en/8/86/The_Silence_of_the_Lambs_poster.jpg",
+		},
 	}
 }
 
