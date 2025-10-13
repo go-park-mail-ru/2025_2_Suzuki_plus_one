4dc278cc5ace423bfe9efeca43222ca1f711cbf2
Fadeev Arsenii <arsmfad@gmail.com>
2025-10-13 13:12:15 +0300

    (feat): added logger messages to main

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	cfg "github.com/go-park-mail-ru/2025_2_Suzuki_plus_one/internal/config"
 	db "github.com/go-park-mail-ru/2025_2_Suzuki_plus_one/internal/db"
 	srv "github.com/go-park-mail-ru/2025_2_Suzuki_plus_one/internal/server"
@@ -9,9 +11,15 @@ import (
 func main() {
 	// Load configuration
 	config := cfg.Load()
+	log.Println("Config loaded")
+
 	database := db.NewDataBase()
+	log.Println("Database created")
+
 	server := srv.NewServer(&config, database)
+	log.Println("Server created")
 
 	// Start the server
 	server.Serve()
+	log.Println("Server started serving")
 }
