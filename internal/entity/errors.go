88e206f505a1277bfe5377a4c82d369e390fe7c1
DinaraGil <dinaragig03@gmail.com>
2025-11-14 18:03:08 +0300

    fix(errors): change error signin message

--- internal/entity/errors.go
+++ internal/entity/errors.go
@@ -9,7 +9,7 @@ var (
 	ErrGetObjectParamsInvalid = errors.New("invalid get_object request parameters")
 	ErrGetObjectFailed        = errors.New("failed to get object from repository")
 
-	ErrPostAuthSignInParamsInvalid         = errors.New("invalid post_auth_sign_in request parameters")
+	ErrPostAuthSignInParamsInvalid         = errors.New("invalid email or password")
 	ErrPostAuthSignInNewRefreshTokenFailed = errors.New("failed to create new refresh token")
 	ErrPostAuthSignInNewAccessTokenFailed  = errors.New("failed to create new access token")
 	ErrPostAuthSignInAddSessionFailed      = errors.New("failed to add session to session repository")
